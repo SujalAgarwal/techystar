483cd88391703f75a56cbba9e8ec3ac92e1597c9
Sujal Agarwal <agarwalsujal211@gmail.com>
2024-01-10 19:44:38 +0530

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Footer() {
+    return <footer>
+        <div>
+            <h1> Techystar</h1>
+            <p> @all rights reserved</p>
+        </div>
+        <div>
+            <h5>
+                Follow us
+            </h5>
+            <div>
+                <a href="https://youtube.com" target={"blank"}>Youtube</a>
+                <a href="https://www.instagram.com/" target={"blank"}>Instagram</a>
+
+                <a href="https://github.com/" target={"blank"}>Github</a>
+            </div>
+        </div>
+    </footer>
+}
+
+export default Footer
